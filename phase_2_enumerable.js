481e150d93dbac920f5a9838ceb83b948e1c3e22
Joey Doyle <j.doyle345@gmail.com>
2018-02-20 23:21:14 -0800

    rewrote myEach in the enumerables

--- phase_2_enumerable.js
+++ phase_2_enumerable.js
@@ -1,26 +1,34 @@
-Array.prototype.myEach= function MyEach(callback){
-  for (let i=0; i < this.length; i++){
-    callback(this[i]);
+// Array.prototype.myEach= function MyEach(callback){
+//   for (let i=0; i < this.length; i++){
+//     callback(this[i]);
+//   }
+// };
+
+Array.prototype.myEach = function(cb) {
+  for (let i = 0; i < this.length; i++) {
+    cb(this[i]);
   }
 };
 
-Array.prototype.myMap = function MyMap(callback){
-  const result = [];
-  this.myEach(function (el){
-    result.push(callback(el));
-  });
-  return result;
-};
+// Array.prototype.myMap = function MyMap(callback){
+//   const result = [];
+//   this.myEach(function (el){
+//     result.push(callback(el));
+//   });
+//   return result;
+// };
 
-Array.prototype.myReduce = function MyReduce(callback, accumulator){
-  let arr=this;
-  if (!accumulator){
-    accumulator=this[0];
-    arr=arr.slice(1);
-  }
-  arr.myEach( function (el) {
-    accumulator = callback(el, accumulator);
-  });
 
-  return accumulator;
-};
+
+// Array.prototype.myReduce = function MyReduce(callback, accumulator){
+//   let arr=this;
+//   if (!accumulator){
+//     accumulator=this[0];
+//     arr=arr.slice(1);
+//   }
+//   arr.myEach( function (el) {
+//     accumulator = callback(el, accumulator);
+//   });
+//
+//   return accumulator;
+// };
